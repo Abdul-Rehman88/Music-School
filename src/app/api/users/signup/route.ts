70a8669553bb89bfc6801318e180926401bc037f
Abdul Rehman <abdulrehmanafridi88@gmail.com>
2026-07-22 14:59:07 +0500

    Add Zod validation schema for user signup and update dependencies

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,13 +2,29 @@ import { NextResponse, NextRequest } from "next/server";
 import { connectDB } from "@/dbConfig/dbConfig";
 import User  from "@/models/userModel";
 import bcryptjs from "bcryptjs";
-
+import { z } from "zod";
 connectDB(); 
+
+const signupSchema = z.object({
+  userName: z.string().min(1, "Username must be at least 1 characters"),
+  email: z.string().email("Invalid email address"),
+  age: z.number().min(5, "Must be at least 5 years old"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+
 export async function POST(request: NextRequest) {
 
     try {
         const requestBody = await request.json();
-        const {userName, email, age, password} = requestBody;
+        const parsed = signupSchema.safeParse(requestBody);
+    
+        if (!parsed.success) {
+          return NextResponse.json(
+            { error: parsed.error.flatten().fieldErrors },{ status: 400 });
+        }
+
+        const {userName, email, age, password} = parsed.data;
         console.log({userName, email, age, password});
 
        const user = await User.findOne({ email})
